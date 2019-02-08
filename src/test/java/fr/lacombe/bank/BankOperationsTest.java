081c05a4e82c2c8d801cfaea62982700fa6afde7
LaCombeDuLionVert_4 <billy>
2019-02-08 15:28:20 +0100

    Test Deposit statement give correct date

--- src/test/java/fr/lacombe/bank/BankOperationsTest.java
+++ src/test/java/fr/lacombe/bank/BankOperationsTest.java
@@ -3,6 +3,7 @@ package fr.lacombe.bank;
 import junitparams.Parameters;
 import org.junit.Test;
 
+import static fr.lacombe.bank.Amount.amountOf;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BankOperationsTest extends AbstractDateMockTest {
@@ -14,5 +15,15 @@ public class BankOperationsTest extends AbstractDateMockTest {
         Account account = new Account();
         String awaitedOperation = String.format("creation;%02d/%02d/%04d;;0.00", day, month, year);
         assertThat(account.operations()).isEqualTo(awaitedOperation);
+    }
+
+    @Test
+    public void operations_should_give_deposit() {
+        mockDateUtilToReturn(2018,12,25);
+        Account account = new Account();
+        mockDateUtilToReturn(2019, 1, 30);
+        account.makeDeposit(amountOf(23.43));
+        assertThat(account.operations()).isEqualTo("creation;25/12/2018;;0.00\n" +
+                "deposit;30/01/2019;23.43;23.43");
     }
 }
