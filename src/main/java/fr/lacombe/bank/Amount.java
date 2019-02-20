7de0da2bd69117e8c0a431ddfeebe93a5b745af4
Xuaveneg <genevauxb@hotmail.fr>
2019-02-20 15:29:45 +0100

    Changing amount to avoid losing precision : passing to int

--- src/main/java/fr/lacombe/bank/Amount.java
+++ src/main/java/fr/lacombe/bank/Amount.java
@@ -6,32 +6,32 @@ import java.util.Objects;
 import static java.lang.Double.compare;
 
 public class Amount {
-    private final double value;
+    private final int cents;
 
-    private Amount(double value) {
-        this.value = roundValue(value);
+    private Amount(int cents) {
+        this.cents = cents;
     }
 
     static Amount amountOf(double value) {
-        return new Amount(value);
+        return new Amount(roundValue(value));
     }
 
     Amount add(Amount amount) {
-        double roundedValue = roundValue(amount.value + value);
-        return Amount.amountOf(roundedValue);
+        return new Amount(amount.cents + cents);
     }
 
-    private double roundValue(double value) {
-        return Math.round(value * 100d) / 100d;
+    private static int roundValue(double value) {
+        return new Double(value * 100d).intValue();
     }
 
     Amount negativeAmount() {
-        return amountOf(-value);
+        return new Amount(-cents);
     }
 
     @Override
     public String toString() {
-        return String.format(Locale.US, "%.2f", value);
+        int positiveCents = Math.abs(cents);
+        return String.format(Locale.US, "%d.%02d", cents / 100, positiveCents % 100);
     }
 
     @Override
@@ -39,11 +39,11 @@ public class Amount {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Amount amount = (Amount) o;
-        return compare(amount.value, value) == 0;
+        return compare(amount.cents, cents) == 0;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(value);
+        return Objects.hash(cents);
     }
 }
